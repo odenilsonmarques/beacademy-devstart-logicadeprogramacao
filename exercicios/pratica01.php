e63f5f4f6cf7a7832f21fe3831f36b36cb1c8faa
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-05-24 22:00:30 -0300

    verificando se o resto da divisao e zero

--- exercicios/pratica01.php
+++ exercicios/pratica01.php
@@ -15,6 +15,22 @@
     $idade = filter_input(INPUT_POST,'idade');
 
     if($nome && $idade){
-        echo "O seu nome é ".$nome." e voce tem ".$idade." anos";
+        echo "O seu nome é ".$nome." e voce tem ".$idade." anos"."<br>";
+
+        if($idade > 18){
+            echo "Verdadeiro"."<br>";
+        }else{
+            echo "falso"."<br>";
+        }
+        if($idade != 25 && $nome == "marcos"){
+            echo "Falso"."<br>";
+        }else if($idade != 25 || $nome == "marcos"){
+            echo "Verdadeiro"."<br>";
+        }
+        if(($idade % 2) == 0){
+            echo "Verdadeiro"."<br>";
+        }else{
+            echo "Falso"."<br>";
+        }
     }
 ?>
