4b1de886a71ebc7fa00e0f81868acbbdeafdc70a
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-05-26 11:40:25 -0300

    pratica 06, calculo da area [200~área e o perímetro

---
+++ exercicios/pratica06.php
@@ -0,0 +1,38 @@
+<!-- Crie um programa que receba do usuário um
+número e apresente a Tabuada deste -->
+
+<form action="" method="POST">
+    <fieldset>
+        <legend>CALCULO DA ÁREA E DO PERÍMETRO</legend>
+        INFORME O VALOR DO RAIO
+        <input type="number" name="valorRaio"><br><br>
+        <input type="submit" value="ACESSAR">
+    </fieldset>
+</form><br>
+
+
+<?php
+$valorRaio = filter_input(INPUT_POST,'valorRaio');
+$pi = 3.1416;
+
+
+
+if($valorRaio){
+    if($valorRaio < 1){
+        echo "Erro! Por favor informe um valor positivo";
+    }
+    else{
+        $calculoArea = $pi * $valorRaio * $valorRaio;
+        $calculoPerimetro = 2 * $pi * $valorRaio;
+
+        echo "o valor da area é ".$calculoArea."<br><br>";
+        echo "o valor da perimetro é ".$calculoPerimetro."<br>";
+
+    }
+}
+
+
+
+
+
+
